b43e1c7423b5acaae7e63a23c58687b93546385c
Lucas Cunha <lucasgalvao260@gmail.com>
2024-03-11 12:43:58 -0300

    Exercisign the table's edit

--- Data Base/index.sql
+++ Data Base/index.sql
@@ -1,11 +1,11 @@
-CREATE TABLE Cars (
+CREATE TABLE cars (
   id INTEGER PRIMARY KEY,
   brand VARCHAR(50),
   model VARCHAR(50),
   value DOUBLE
 );
 
-CREATE TABLE Sales (
+CREATE TABLE sales (
   id INTEGER PRIMARY KEY,
   car_id INTEGER,
   client_id INTEGER,
@@ -15,10 +15,19 @@ CREATE TABLE Sales (
   FOREIGN KEY (client_id) REFERENCES Clients(id)
 );
 
-CREATE TABLE Clients (
+CREATE TABLE clients (
   id INTEGER PRIMARY KEY,
   Name VARCHAR(200),
   email VARCHAR(200),
   phone INTEGER,
   address VARCHAR(200)
 );
+CREATE TABLE prices (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    product_name VARCHAR(255),
+    price DECIMAL(10, 2)
+);
+ALTER TABLE clients 
+  ADD COLUMN  age INTEGER ;
+
+DROP TABLE prices
